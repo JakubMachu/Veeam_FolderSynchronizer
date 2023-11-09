319235356f7485fcb93049cbcd993e96a2e6163c
JakubMachu <jacubmachu@gmail.com>
2023-11-09 10:43:24 +0100

    Second commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Security.Cryptography;
+using System.Linq.Expressions;
 
 namespace Veeam_FolderSynchronizer
 {
@@ -12,7 +13,7 @@ namespace Veeam_FolderSynchronizer
     {
         static void Main(string[] args)
         {
-            if (args.Length < 3) 
+            if (args.Length < 3)
             {
                 Console.WriteLine("Warning: Wrong usage!");
                 Console.WriteLine("Correct usage: FolderSync.exe \"<sourceFolder>\" \"<replicaFolder>\" <syncInterval> \"<logFile>\"")
@@ -20,7 +21,7 @@ namespace Veeam_FolderSynchronizer
             }
 
             var sourceFolder = args[0];
-            var replicaFolder = args[1];    
+            var replicaFolder = args[1];
             var syncInterval = int.Parse(args[2]);
             var logFile = args[3];
 
@@ -37,9 +38,10 @@ namespace Veeam_FolderSynchronizer
         {
             try
             {
+                //Source files section\\
                 var[] sourceFiles = Directory.GetFiles(sourceFolder, "*", SearchOption.AllDirectories);
 
-                foreach(var sourceFile in sourceFiles)
+                foreach (var sourceFile in sourceFiles)
                 {
                     var relativePath = sourceFile.Substring.(sourceFolder.Length);
                     var replicaFile = replicaFolder + relativePath;
@@ -47,13 +49,30 @@ namespace Veeam_FolderSynchronizer
                     if (!File.Exists(replicaFile) || !AreaFilesEqual(sourceFile, replicaFile))
                     {
                         Directory.CreateDirectory(Path.GetDirectoryName(replicaFile));
-                        File.Copy(sourceFile, replicaFile, true);   
+                        File.Copy(sourceFile, replicaFile, true);
                         LogAction.(logFile, $"Copying a file: {sourceFile} --> {replicaFile}");
                     }
                 }
 
-                
+                //Replica files section\\
+                var[] replicaFiles = Directory.GetFiles(replicaFolder, "*", SearchOption.AllDirectories);
+
+                foreach (var replicaFile in replicaFiles)
+                {
+                    var relativePath = replicaFile.Substring(replicaFolder.Length);
+                    var sourceFile = sourceFolder + relativePath;
+
+                    if (File.Exists(sourceFile))
+                    {
+                        File.Delete(replicaFile);
+                        LogAction(logFile, $"Deleting a file: {replicaFile}");
+                    }
+                }
             }
-        }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"There were a problem during synchronization of: {sourceFolder} and {replicaFolder} --> {ex.Message}");
+            }
+        } 
     }
 }
